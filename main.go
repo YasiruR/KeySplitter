a4246ca6634a325fbb9c855954b5fb09da2c0f9c
Yasi <wyasirurathsara@gmail.com>
2024-02-11 15:32:15 +0100

    :zap: rm helper code in main

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"github.com/YasiruR/keySplitter/domain"
 	"github.com/YasiruR/keySplitter/services"
-	"strconv"
 )
 
 func main() {
@@ -15,40 +14,4 @@ func main() {
 
 	m := services.NewMerger()
 	fmt.Println(`res secret: `, string(m.Merge(shares)))
-}
-
-func addN(a, b int) int {
-	var cStr string
-	aStr, bStr := strconv.Itoa(a), strconv.Itoa(b)
-
-	// Adjusting numbers into same lengths by adding leading zeros
-	var excDigits int
-	if len(aStr) >= len(bStr) {
-		excDigits = len(aStr) - len(bStr)
-		for i := 0; i < excDigits; i++ {
-			bStr = `0` + bStr
-		}
-	} else {
-		excDigits = len(bStr) - len(aStr)
-		for i := 0; i < excDigits; i++ {
-			aStr = `0` + aStr
-		}
-	}
-
-	for i, _ := range aStr {
-		digA, _ := strconv.Atoi(string(aStr[i]))
-		digB, _ := strconv.Atoi(string(bStr[i]))
-		cStr += strconv.Itoa(addD(digA, digB))
-	}
-
-	c, _ := strconv.Atoi(cStr)
-	return c
-}
-
-func addD(a, b int) int {
-	sum := a + b
-	if sum > 9 {
-		return sum - 10
-	}
-	return sum
 }
